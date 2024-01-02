7af0624c8936e24a3b74e036ed606c25458b9955
kenkindermann <ken.kindermann@gmail.com>
2024-01-02 17:44:02 +0100

    add unique key and basic style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import NewsFeed from "./components/NewsFeed";
 import SearchBar from "./components/SearchBar";
 
